d28419c25cc77bb939a4197c1b641a963ca94ac6
Arthur <alt-arti@mail.ru>
2023-01-06 15:29:17 +0300

    added 2nd screen

--- app/src/main/java/com/example/aboutbank/MainActivity.kt
+++ app/src/main/java/com/example/aboutbank/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.aboutbank
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Инициализировали объект кнопки
+        // Указали объект кнопки на элемент внутри XML
+        val registerButton = findViewById<Button>(R.id.activity_main_Button_register)
+        // Прикрепили обработчик нажатия к нашей кнопке
+        registerButton.setOnClickListener {
+            val intentToAnotherScreen = Intent(this@MainActivity, RegistrationActivity::class.java)
+            startActivity(intentToAnotherScreen)
+            Toast.makeText(this@MainActivity, "You clicked me", Toast.LENGTH_SHORT).show()
+        }
     }
 }
